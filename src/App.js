06e6ea0ba6128a6c796665f7fb5d7eec3de1ee08
Lorenzo Baldassari <lorebalda93@gmail.com>
2024-02-20 14:58:03 +0100

    post finiti e inizio pagine

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
 import Register from "./Components/Register";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CustomNavbar from "./Components/Navbar";
 import LoginPagina from "./Components/LoginPagine";
 import Wellcome from "./Components/Posts/Wellcome";
+import Profilo from "./Components/Profilo/Profilo";
+import ProfiloPagina from "./Components/Profilo/ProfiloPagina";
+import Pagine from "./Components/Posts/Pagine/Pagine";
+// import ProfiloSingolaPagina from "../src/Components/Profilo/PRofiloSingolaPagina";
 
 function App() {
   return (
@@ -20,6 +23,10 @@ function App() {
           <Route path="/paginaLogin" Component={LoginPagina} />
           <Route path="/register" Component={Register} />
           <Route path="/home" Component={Home} />
+          <Route path="/profilo/:id" Component={Profilo} />
+          <Route path="/profiloPagina/:id" Component={ProfiloPagina} />
+          <Route path="/pagine" Component={Pagine} />
+          {/* <Route path="/singolaPagina" Component={ProfiloSingolaPagina} /> */}
         </Routes>
       </BrowserRouter>
     </div>
